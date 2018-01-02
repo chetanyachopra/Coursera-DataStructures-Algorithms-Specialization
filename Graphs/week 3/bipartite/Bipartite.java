796dab8c5226a5cd01eb3e3ec3788ce027ad447c
chetanyachopra <devchetanyachopra@gmail.com>
2018-01-02 19:45:31 +0530

    implimented Bipertite and completed week 3

--- Graphs/week 3/bipartite/Bipartite.java
+++ Graphs/week 3/bipartite/Bipartite.java
@@ -1,12 +1,43 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Bipartite {
+
+    private static int fillColor(ArrayList<Integer>[] adj, int[] color) {
+        for(int src = 0; src < adj.length;src++) {
+        //int src = 0;
+            Queue<Integer> queue = new LinkedList<Integer>();
+
+            if(color[src] == -1) {
+                queue.add(src);
+                color[src] = 1;
+
+                while(queue.size() != 0) {
+                    int u = queue.poll();
+
+                    Iterator<Integer> it = adj[u].listIterator();
+                    while(it.hasNext()) {
+                        int n = it.next();
+                        if(color[n] == -1) {
+                            color[n] = 1 - color[u];
+                            queue.add(n);
+                        }
+                        else if(color[n] == color[u])return 0;
+                    }
+                }
+            }
+        }
+        return 1;
+    }
+
     private static int bipartite(ArrayList<Integer>[] adj) {
-        //write your code here
-        return -1;
+        int[] color = new int[adj.length];
+        Arrays.fill(color, -1);
+
+        return fillColor(adj, color);
+       // return (checkColor(adj, color) == true) ? 1 : 0;
+        //for(int i = 0;i < color.length;i++)
+            //System.out.print(color[i] + " ");
+        //return -1;
     }
 
     public static void main(String[] args) {
@@ -24,7 +55,7 @@ public class Bipartite {
             adj[x - 1].add(y - 1);
             adj[y - 1].add(x - 1);
         }
-        System.out.println(bipartite(adj));
+        System.out.println("\n" + bipartite(adj));
     }
 }
 
